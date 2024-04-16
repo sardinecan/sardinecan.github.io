a42caee057e15295eef755ecc4de969060cd701d
sardinecan <morvan.josselin@gmail.com>
2024-04-16 11:09:23 +0200

    added md

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Julia Cheatsheet by Sardinecan</h1>
+<ul>
+  <li><a href="/basics">Basics</a></li>
+</ul>
