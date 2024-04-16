26b7d6ef64089c9a3818510dfce36e57f5ae8adb
sardinecan <morvan.josselin@gmail.com>
2024-04-16 13:02:31 +0200

    added languages

--- src/routes/contact/+page.svelte
+++
@@ -1,5 +0,0 @@
-<h1>Contact</h1>
-<ul>
-  <li><a href="/">home</a></li>
-  <li><a href="mail">Mail</a></li>
-</ul>
---
+++ src/routes/languages/julia/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let data;
+</script>
+
+<h1>Langage / Julia</h1>
+<ul>
+	{#each data.posts as post}
+		<li>
+			<h2>
+				<a href={post.path}>
+					{post.meta.title}
+				</a>
+			</h2>
+			Published {post.meta.date}
+		</li>
+	{/each}
+</ul>
