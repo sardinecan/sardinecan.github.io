4f285311ea3800e0322c4ae4648368a8efe4885c
sardinecan <morvan.josselin@gmail.com>
2024-04-16 14:43:06 +0200

    added reviewed js var

--- src/routes/languages/julia/+page.js
+++ src/routes/languages/julia/+page.js
@@ -1,7 +1,7 @@
 // src/routes/blog/+page.js
 export const load = async ({ fetch }) => {
 	const response = await fetch(`/api/languages/julia`);
-	const posts = await response.json();
+	const mdFiles = await response.json();
 
 	return {
 		mdFiles
