8c7d3c4ed17ae56284f837a3142c9ba0214ea061
sardinecan <morvan.josselin@gmail.com>
2024-04-16 14:51:18 +0200

    renamed html folders to xhtml

---
+++ src/routes/api/languages/xhtml/+server.js
@@ -0,0 +1,12 @@
+import { fetchHtmlFiles } from '$lib/utils';
+import { json } from '@sveltejs/kit';
+
+export const GET = async () => {
+	const allPosts = await fetchHtmlFiles();
+
+	const sortedPosts = allPosts.sort((a, b) => {
+		return new Date(b.meta.date) - new Date(a.meta.date);
+	});
+
+	return json(sortedPosts);
+};
--- src/routes/languages/+page.svelte
+++ src/routes/languages/+page.svelte
@@ -2,6 +2,6 @@
 <nav>
   <ul>
     <li><a href="/languages/julia">Julia</a></li>
-    <li><a href="/languages/html">HTML</a></li>
+    <li><a href="/languages/xhtml">xHTML</a></li>
   </ul>
 </nav>
