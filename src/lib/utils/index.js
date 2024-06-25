1142a9c3f25544120182b5e356b33fbbfeb8fcae
sardinecan <morvan.josselin@gmail.com>
2024-06-25 23:09:47 +0200

    added index function for cheatsheets

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -131,3 +131,29 @@ export const fetchNotesFiles = async () => {
 
 	return allFiles;
 };
+
+export const fetchCheatsheetFiles = async () => {
+    const allMarkdownFiles = import.meta.glob('/src/routes/cheatsheets/*/*.md');
+    const iterableMarkdownFiles = Object.entries(allMarkdownFiles);
+
+    const allFiles = await Promise.all(
+        iterableMarkdownFiles.map(async ([path, resolver]) => {
+            const { metadata } = await resolver();
+
+            // Utilisation d'une expression régulière pour capturer la valeur de la première étoile
+            const match = path.match(/\/cheatsheets\/([^/]+)\//);
+            const category = match ? match[1] : null;
+
+            // Extraction du chemin sans le prefix et suffix
+            const filePath = path.slice(11, -3);
+
+            return {
+                meta: metadata,
+                path: filePath,
+                category: category // Ajout de la catégorie
+            };
+        })
+    );
+
+    return allFiles;
+};
