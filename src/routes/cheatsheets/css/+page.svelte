0c8350652440aa7a5eeb2c9dcaed65d36947d400
sardinecan <morvan.josselin@gmail.com>
2024-06-28 11:42:13 +0200

    added redirection when only 1 cheatsheet

--- src/routes/cheatsheets/css/+page.svelte
+++ src/routes/cheatsheets/css/+page.svelte
@@ -1,16 +1,23 @@
 <script>
 	export let data;
 	import { dateToString } from '$lib/utils/dates.js';
-	import { page } from '$app/stores';  
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
 
 	let tokenizedPath = $page.url.pathname.split('/');
 	let folder = tokenizedPath[2];
-	let visibleData = data.mdFiles.filter(mdFile => mdFile.category === folder);
+	let filteredData = data.mdFiles.filter(mdFile => mdFile.category === folder);
+	let numberOfFilteredData = filteredData.length; 
+	console.log("nb de fichiers", numberOfFilteredData);
+	if(numberOfFilteredData == 1) {
+		console.log("redirection vers : ", filteredData[0].path);
+		goto(filteredData[0].path);
+	};
 </script>
 
 <h1>{ folder.toUpperCase() }</h1>
 <ul class="cards">
-	{#each visibleData as mdFile}
+	{#each filteredData as mdFile}
 		<li>
 			<a href="{mdFile.path}">
 				<article>
