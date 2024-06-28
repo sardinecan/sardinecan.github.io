2e5e00296f0c1a1e4b52e7efd347731f63ba07c1
sardinecan <morvan.josselin@gmail.com>
2024-06-28 14:04:49 +0200

    reviewed history when redirection

--- src/routes/cheatsheets/sql/+page.svelte
+++ src/routes/cheatsheets/sql/+page.svelte
@@ -3,17 +3,19 @@
 	import { dateToString } from '$lib/utils/dates.js';
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
 	let tokenizedPath = $page.url.pathname.split('/');
 	let folder = tokenizedPath[2];
 	let filteredData = data.mdFiles.filter(mdFile => mdFile.category === folder);
 	let numberOfFilteredData = filteredData.length;
 	console.log("nb de fichiers", numberOfFilteredData);
-	
-	if(numberOfFilteredData == 1) {
-		console.log("redirection vers : ", filteredData[0].path);
-		goto(filteredData[0].path);
-	};
+	onMount(() => {
+		if(numberOfFilteredData == 1) {
+			console.log("redirection vers : ", filteredData[0].path);
+			goto(filteredData[0].path, { replaceState: true });
+		}
+	});
 </script>
 
 <h1>{ folder.toUpperCase() }</h1>
